b3c8768553579d1236a0a8564a0c96b04e496eb0
Yusuf Çakal <yusuf.cakal@dolap.com>
2020-12-23 14:29:02 +0300

    added script js path

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+new Typed('.js-about-info', {
+  strings: [
+    'java developer',
+    'addicted test oriented development',
+    'lifelong learner'
+  ],
+  typeSpeed: 50,
+  loop: true,
+  backDelay: 1200
+});
